f0d3319db07fe038050479569d0fc2c365292b21
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-10-06 08:21:26 +0100

    ''

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+// app.use(express.json());
+// //cors
+//app.use(cors());
+
+//mongoose
+mongoose.connect(process.env.DB_URI);
+const db = mongoose.connection;
+db.on("on", () => console.log("Connected to db"));
+db.on("error", (err) => console.log("Connected to db"));
+
+// cors
 
 app.listen(3001, () => {
   console.log("Running");
 });
+
+//  Routes
+//users routes
+// const usersRoutes = require("./routes/users");
+// app.use("/users", usersRoutes);
