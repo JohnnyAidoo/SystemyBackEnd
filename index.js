d4a4166c378bc51280bdb4f4886b37321198b8e3
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-10-07 15:56:37 +0100

    'added user routes'

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-// app.use(express.json());
-// //cors
-//app.use(cors());
+app.use(express.json());
+//cors
+app.use(cors());
 
 //mongoose
-mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI).then(() => {
+  console.log("connected to db");
+});
 const db = mongoose.connection;
-db.on("on", () => console.log("Connected to db"));
-db.on("error", (err) => console.log("Connected to db"));
 
-// cors
+//  Routes
+// users routes
+const usersRoutes = require("./routes/users");
+app.use("/users", usersRoutes);
 
 app.listen(3001, () => {
   console.log("Running");
 });
-
-//  Routes
-//users routes
-// const usersRoutes = require("./routes/users");
-// app.use("/users", usersRoutes);
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,65 @@
 const express = require("express");
+const usersModel = require("../models/users");
 const router = express.Router();
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 
+// get Users
+
+router.get("/", async (req, res) => {
+  try {
+    usersModel.find().then((users) => {
+      res.json(users);
+    });
+  } catch (err) {
+    req.status(500).json(err);
+  }
+});
+
+// get on user
+
+router.get("/:id", async (req, res) => {
+  try {
+    usersModel.findById(req.params.id).then((user) => {
+      res.json(user);
+    });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// add user
+//todo add password hashing
+
+router.post("/add", (req, res) => {
+  userName = req.body.userName;
+  password = req.body.password;
+
+  const newUser = new usersModel({
+    userName: userName,
+    password: password,
+  });
+  try {
+    newUser.save().then((response) => {
+      res.json(response);
+    });
+  } catch (err) {
+    res.jon(err);
+  }
+});
+
+//TODO update user
+
+//todo google sign in
+router.get("/googleSign", (req, res) => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth;
+  signInWithPopup(auth, provider).then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    const user = result.user;
+    res.json(user);
+  });
+});
+
+module.exports = router;
